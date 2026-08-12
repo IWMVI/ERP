4d020f0111d4268e3760d2f31970c0919dd38225
Wallace Martins <wmvwallace@gmail.com>
2026-08-11 23:29:13 -0300

    feat: Inicializa projeto Spring Boot com Maven Wrapper, PostgreSQL setup, e estrutura básica.

---
+++ src/test/java/iwmvi/erp/ErpApplicationTests.java
@@ -0,0 +1,13 @@
+package iwmvi.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
