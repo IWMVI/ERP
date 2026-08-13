c818a72a3e064ee3c9bf3b2e9e98c1c5047a0e16
Wallace Martins <108425466+IWMVI@users.noreply.github.com>
2026-08-13 00:34:44 -0300

    feat: adiciona ordenação na listagem de usuários

--- src/main/java/iwmvi/erp/usuario/UsuarioRepository.java
+++ src/main/java/iwmvi/erp/usuario/UsuarioRepository.java
@@ -1,11 +1,12 @@
 package iwmvi.erp.usuario;
 
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
     boolean existsByEmail(String email);
-
     Optional<Usuario> findByEmail(String email);
+    List<Usuario> findAllByOrderByNomeAsc();
 }
