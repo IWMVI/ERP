993847fbf6b4373b74c77658c9fa1c6d1939c382
Wallace Martins <108425466+IWMVI@users.noreply.github.com>
2026-08-13 00:06:20 -0300

    feat: adiciona mapper de usuario

---
+++ src/main/java/iwmvi/erp/usuario/UsuarioMapper.java
@@ -0,0 +1,16 @@
+package iwmvi.erp.usuario;
+
+public final class UsuarioMapper {
+
+    private UsuarioMapper() {
+    }
+
+    public static UsuarioResponse toResponse(Usuario usuario) {
+        return new UsuarioResponse(
+                usuario.getId(),
+                usuario.getNome(),
+                usuario.getEmail(),
+                usuario.isAtivo()
+        );
+    }
+}
