b7bfccf686598ec5652e5da23326613748b3fc65
Wallace Martins <108425466+IWMVI@users.noreply.github.com>
2026-08-15 22:22:52 -0300

    test: valida identificação automática do tipo de pessoa

---
+++ src/test/java/iwmvi/erp/cliente/ClienteRequestTipoPessoaTest.java
@@ -0,0 +1,49 @@
+package iwmvi.erp.cliente;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.Test;
+
+class ClienteRequestTipoPessoaTest {
+
+    @Test
+    void deveIdentificarPessoaFisicaPeloCpf() {
+        ClienteRequest request = request("529.982.247-25");
+
+        assertEquals(TipoPessoa.FISICA, request.tipoPessoa());
+    }
+
+    @Test
+    void deveIdentificarPessoaJuridicaPeloCnpj() {
+        ClienteRequest request = request("19.131.243/0001-97");
+
+        assertEquals(TipoPessoa.JURIDICA, request.tipoPessoa());
+    }
+
+    @Test
+    void naoDeveDefinirTipoParaDocumentoIncompleto() {
+        ClienteRequest request = request("12345");
+
+        assertNull(request.tipoPessoa());
+    }
+
+    private ClienteRequest request(String documento) {
+        return new ClienteRequest(
+                null,
+                "Teste",
+                "",
+                documento,
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "");
+    }
+}
